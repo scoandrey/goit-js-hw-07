bdbc328aeffcdbb83bd7514fd87158abd367ee42
scoandrey <32551449+scoandrey@users.noreply.github.com>
2023-03-25 18:00:53 -0400

    save

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,34 +2,41 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryRef = document.querySelector(".gallery");
-// const markUp = createMarkUp(galleryItems);
 galleryRef.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
 
 function createMarkUp(galleryItems) {
   const markUp = galleryItems
-    .map(({ preview, original, description }) => {
-      return '<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source= "${original}" alt="${description}"/></a></li>';
+    .map(({ preview, original }) => {
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source= "${original}" alt="Image description"/></a></li>`;
     })
     .join("");
   return markUp;
 }
 
-// galleryRef.addEventListener("click", (e) => {
-//   if (e.target.nodeName !== "IMG") return;
-// });
+galleryRef.addEventListener("click", (e) => {
+  onModal(e);
+  e.preventDefault();
+  if (e.target.nodeName === "IMG");
+  return;
+});
 
-// document.querySelector('button.image').onclick = () => {
+function onModal(e) {
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.sourse}"/>`,
+    {
+      onShow: (_instance) => {
+        galleryRef.addEventListener("keydown", onEscBtn);
+      },
+      onClose: (_instance) => {
+        galleryRef.removeEventListener("keydown", onEscBtn);
+      },
+    }
+  );
+  instance.show();
 
-// 	basicLightbox.create(`
-// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
-// 	`).show()
-
-// }
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-
-// instance.show()
-
-console.log(galleryItems);
+  function onEscBtn(e) {
+    if (e.code === "Escape") {
+    }
+    instance.close();
+  }
+}
