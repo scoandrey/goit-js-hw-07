cb7f5eb1abfad0f9876daace45f1fb8d2c8f4c7c
scoandrey <32551449+scoandrey@users.noreply.github.com>
2023-03-26 23:07:15 -0400

    hw-7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+
+function createMarkUp(galleryItems) {
+  const markUp = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+    })
+    .join("");
+  return markUp;
+}
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+  onModal(e);
+});
+
+function onModal(e) {
+  console.log(e.target);
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  const instance = SimpleLightbox.create(
+    `<img src="${e.target.dataset.source}"/>`,
+    {
+      onShow: (_instance) => {
+        galleryRef.addEventListener("keydown", onEscBtn);
+      },
+      onClose: (_instance) => {
+        galleryRef.removeEventListener("keydown", onEscBtn);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscBtn(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
