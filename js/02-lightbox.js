36c14ca8e26517a0146034beb2a94c4157203665
scoandrey <32551449+scoandrey@users.noreply.github.com>
2023-03-29 22:09:29 -0400

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,35 +16,7 @@ function createMarkUp(galleryItems) {
     .join("");
   return markUp;
 }
-
-galleryRef.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (e.target.nodeName !== "IMG") return;
-  onModal(e);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
-
-function onModal(e) {
-  console.log(e.target);
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-  const instance = SimpleLightbox.create(
-    `<img src="${e.target.dataset.source}"/>`,
-    {
-      onShow: (_instance) => {
-        galleryRef.addEventListener("keydown", onEscBtn);
-      },
-      onClose: (_instance) => {
-        galleryRef.removeEventListener("keydown", onEscBtn);
-      },
-    }
-  );
-  instance.show();
-
-  function onEscBtn(e) {
-    if (e.code === "Escape") {
-      instance.close();
-    }
-  }
-}
